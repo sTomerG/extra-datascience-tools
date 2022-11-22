4ff09c9a1d500d2e803049878b2e3b54d7b644fe
Tomer Gabay <tomergabay@Admins-MacBook-Pro.local>
2022-11-22 14:21:38 +0100

    wrote tests for filter_tried_params

---
+++ tests/test_ml/test_model_selection/test_filter_tried_params.py
@@ -0,0 +1,17 @@
+import pytest
+from extra_ds_tools.ml.sklearn.model_selection import filter_tried_params
+
+
+@pytest.mark.parametrize(
+    "gridsearches",
+    [
+        "gridsearchcvs_tree_dict",
+        "gridsearchcvs_tree_list",
+        "gridsearchcvs_tree_dictlist",
+        "gridsearchcvs_tree_listdict",
+    ],
+)
+def test_filter_tried_params_dict(gridsearches, request):
+    new_gridsearch, *tried_gridsearches = request.getfixturevalue(gridsearches)
+    new_params = filter_tried_params(new_gridsearch, tried_gridsearches)
+    assert len(new_params) == 2
